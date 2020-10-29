4595880278112d9a21b80d78a316540ad7fdeab7
Reis-Silva J.C <cesar.everlastingtechnologydeveloper@outlook.com>
2020-10-28 23:49:34 -0300

    inserção dos detalhes dos deputados

--- deputadoscamara/src/main/java/entity/copy/Deputados.java
+++ deputadoscamara/src/main/java/entity/copy/Deputados.java
@@ -1,5 +1,73 @@
 package entity.copy;
 
+
 public class Deputados {
 
+	private int id;
+	private String uri;
+	private String nome;
+	private String siglaPartido;
+	private String uriPartido;
+	private String siglaUf;
+	private int idLegilatura;
+	private String urlFoto;
+	private String email;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUri() {
+		return uri;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSiglaPartido() {
+		return siglaPartido;
+	}
+	public void setSiglaPartido(String siglaPartido) {
+		this.siglaPartido = siglaPartido;
+	}
+	public String getUriPartido() {
+		return uriPartido;
+	}
+	public void setUriPartido(String uriPartido) {
+		this.uriPartido = uriPartido;
+	}
+	public String getSiglaUf() {
+		return siglaUf;
+	}
+	public void setSiglaUf(String siglaUf) {
+		this.siglaUf = siglaUf;
+	}
+	public int getIdLegilatura() {
+		return idLegilatura;
+	}
+	public void setIdLegilatura(int idLegilatura) {
+		this.idLegilatura = idLegilatura;
+	}
+	public String getUrlFoto() {
+		return urlFoto;
+	}
+	public void setUrlFoto(String urlFoto) {
+		this.urlFoto = urlFoto;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
